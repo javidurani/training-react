9c67b4ebe0ccb746a5d1c15daf77174b863d9382
javidurani <jduraniglesias21122@columbushs.com>
2021-02-01 09:39:35 -0800

    first init (idcard)

---
+++ src/IdCard.js
@@ -0,0 +1,10 @@
+function IdCard(){
+    <IdCard
+        lastName = 'Duran'
+        firstName = 'Javi'
+        gender = 'male'
+        height={1000}
+        birth = {new Date("2003-04-16")}
+        picture="https://ca.slack-edge.com/TM2PP13R7-UMJ6EEUDA-556101467d95-512"
+    />
+}
